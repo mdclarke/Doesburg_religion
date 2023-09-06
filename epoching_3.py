dab79e3d09dea06d02d0d52f81d8552e5ccd7eb1
Maggie Clarke <mdclarke@uw.edu>
2023-09-06 12:10:04 -0700

    Create epoching_3.py

---
+++ epoching_3.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  6 11:43:57 2023
+
+@author: maggie
+"""
+
+import mne
+import os.path as op
+
+####### set these  before running #############################################
+path = '/home/maggie/data/sam/'
+
+# setup subject name ### change this for each subject
+s = 'pilot_4'
+c = 'task_1_raw'
+###############################################################################
+
+# read in processed data
+filename =  op.join(path, '%s' %s,  '%s_tsss_ica.fif' %c)
+tsss = mne.io.Raw(filename, preload=True)
+
+# find triggers
+events = mne.find_events(tsss, stim_channel='STI101', 
+                         shortest_event=1/tsss.info['sfreq'])
+
+# define trigger of interest
+event_dict = {
+  "human": 131,
+  "inan": 121,
+  "god": 111,
+  "super":141
+    }
+
+# plot triggers (events)
+fig = mne.viz.plot_events(
+    events, event_id=event_dict, sfreq=tsss.info["sfreq"], 
+    first_samp=tsss.first_samp)
+fig.savefig(op.join(path, '%s' %s, 'meg', 'figures', '%s_events' %c))
+
+#reject mags and grads > 9000 fT, 4000 fT/cm
+reject = dict(mag=9000e-15, grad=4000e-13)
+
+# epoch data w respect to event onset
+epochs = mne.Epochs(tsss, events, event_id=event_dict,
+                    tmin=-0.1, tmax=0.5, baseline=(-0.1, 0), 
+                    reject=reject, preload=True)
+# plot epochs
+epochs.plot(events=events)
+
+# plot drop log - this is how many epochs got dropped from the 
+# amplitude rejection
+fig = epochs.plot_drop_log()
+fig.savefig(op.join(path, '%s' %s, 'meg', 'figures', '%s_droplog' %c))
+
+# average epoched data
+human = epochs["human"].average()
+inan = epochs["inan"].average()
+god = epochs["god"].average()
+super = epochs["super"].average()
+
+# plot average
+human.plot_joint()
+inan.plot_joint()
+god.plot_joint()
+super.plot_joint()
